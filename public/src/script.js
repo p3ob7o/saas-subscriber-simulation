ba081e327c6784d5ca1d560f6d4a9a39836d7b99
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-09-17 01:31:49 +0200

    Update script.js

--- public/src/script.js
+++ public/src/script.js
@@ -2,7 +2,7 @@ function calculateSubscribers(acquisitionGrowth, monthlyChurnRate) {
   const subscribers = [50000];
   let newSubscribers = 1500;
 
-  for (let i = 1; i <= 36; i++) {
+  for (let i = 1; i <= 60; i++) {
       newSubscribers += newSubscribers * (acquisitionGrowth / 100);
       let churningSubscribers = subscribers[i - 1] * (monthlyChurnRate / 100);
       subscribers[i] = subscribers[i - 1] + newSubscribers - churningSubscribers;
@@ -23,7 +23,7 @@ function renderChart(data) {
   subscriberChart = new Chart(ctx, {
       type: 'line',
       data: {
-          labels: [...Array(37).keys()],
+          labels: [...Array(61).keys()],
           datasets: [{
               label: 'Number of Subscribers',
               data: data,
