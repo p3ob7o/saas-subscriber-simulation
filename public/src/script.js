cb7cafa7cf1aebd5750bf7803cd00a0fc81b4f84
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-09-17 01:12:57 +0200

    Update values

--- public/src/script.js
+++ public/src/script.js
@@ -1,67 +1,66 @@
 function calculateSubscribers(acquisitionGrowth, churnReduction) {
-    const subscribers = [50000];
-    let newSubscribers = 1500;
-    let churningSubscribers = 1500;
-  
-    for (let i = 1; i <= 36; i++) {
-      newSubscribers += newSubscribers * (acquisitionGrowth / 100);
-      churningSubscribers -= churningSubscribers * (churnReduction / 100);
-      subscribers[i] = subscribers[i - 1] + newSubscribers - churningSubscribers;
-    }
-  
-    return subscribers;
+  const subscribers = [50000];
+  let newSubscribers = 1500;
+  let churningSubscribers = 1500;
+
+  for (let i = 1; i <= 36; i++) {
+    newSubscribers += newSubscribers * (acquisitionGrowth / 100);
+    churningSubscribers -= churningSubscribers * (churnReduction / 100);
+    subscribers[i] = subscribers[i - 1] + newSubscribers - churningSubscribers;
   }
-  
-  let subscriberChart;
+
+  return subscribers;
+}
+
+let subscriberChart;
 
 function renderChart(data) {
   const ctx = document.getElementById('subscriberChart').getContext('2d');
 
   if (subscriberChart) {
-    subscriberChart.destroy();
+      subscriberChart.destroy();
   }
 
   subscriberChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [...Array(37).keys()],
-      datasets: [{
-        label: 'Number of Subscribers',
-        data: data,
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        x: {
-          beginAtZero: true
-        },
-        y: {
-          beginAtZero: true
-        }
+      type: 'line',
+      data: {
+          labels: [...Array(37).keys()],
+          datasets: [{
+              label: 'Number of Subscribers',
+              data: data,
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              x: {
+                  beginAtZero: true
+              },
+              y: {
+                  beginAtZero: true
+              }
+          }
       }
-    }
   });
 }
 
 function updateChart() {
-    const acquisitionGrowth = parseFloat(document.getElementById('acquisitionGrowth').value);
-    const churnReduction = parseFloat(document.getElementById('churnReduction').value);
-    
-    const newSubscribersData = calculateSubscribers(acquisitionGrowth, churnReduction);
-    renderChart(newSubscribersData);
-  }
+  const acquisitionGrowth = parseFloat(document.getElementById('acquisitionGrowth').value);
+  const churnReduction = parseFloat(document.getElementById('churnReduction').value);
 
-  document.getElementById('acquisitionGrowth').oninput = function() {
-    document.getElementById('acquisitionOutput').value = this.value;
+  const newSubscribersData = calculateSubscribers(acquisitionGrowth, churnReduction);
+  renderChart(newSubscribersData);
+}
+
+document.getElementById('acquisitionGrowth').oninput = function() {
+  document.getElementById('acquisitionOutput').textContent = this.value;
 }
 
 document.getElementById('churnReduction').oninput = function() {
-    document.getElementById('churnOutput').value = this.value;
+  document.getElementById('churnOutput').textContent = this.value;
 }
 
-  
-  window.onload = () => {
-    updateChart();
-  };
+window.onload = () => {
+  updateChart();
+};
