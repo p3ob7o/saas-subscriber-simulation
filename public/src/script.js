2994cbc0f3b84c28521639225b09c29055c90db9
Paolo Belcastro <p3ob7o@users.noreply.github.com>
2023-09-18 00:48:10 +0200

    Refactored JS

--- public/src/script.js
+++ public/src/script.js
@@ -1,55 +1,34 @@
 let startingSubscribers = 50000;
 let newSubscribersPerMonth = 1500;
-let initialMonthlyChurnRate = 5;
+let initialMonthlyChurnRate = 4;
 
-function calculateSubscribers(acquisitionGrowth, monthlyChurnRate) {
-  const subscribers = [startingSubscribers];
-  let newSubscribers = newSubscribersPerMonth;
-
-  for (let i = 1; i <= 60; i++) {
-    newSubscribers += newSubscribers * (acquisitionGrowth / 100);
-    let churningSubscribers = subscribers[i - 1] * (monthlyChurnRate / 100);
-    subscribers[i] = subscribers[i - 1] + newSubscribers - churningSubscribers;
+function calculateMetrics(startingSubscribers, newSubscribersPerMonth, acquisitionGrowth, monthlyChurnRate) {
+    const totalSubscribers = [startingSubscribers];
+    const newSubscribersData = [Math.round(newSubscribersPerMonth)];
+    const churnedSubscribers = [startingSubscribers * (monthlyChurnRate / 100)]; // Calculating churn for the first month
+    const growthRates = [null, null, null, null, null, null, null, null, null, null, null, null];
+  
+    let newSubscribers = newSubscribersPerMonth;
+  
+    for (let i = 1; i <= 60; i++) {
+      newSubscribers += newSubscribers * (acquisitionGrowth / 100);
+      let churningSubscribers = totalSubscribers[i - 1] * (monthlyChurnRate / 100);
+  
+      totalSubscribers[i] = totalSubscribers[i - 1] + newSubscribers - churningSubscribers;
+      newSubscribersData[i] = newSubscribers;
+      churnedSubscribers[i] = churningSubscribers;
+    }
+  
+    for (let i = 12; i < totalSubscribers.length; i++) {
+      growthRates[i] = ((totalSubscribers[i] / totalSubscribers[i - 12]) - 1) * 100;
+    }
+  
+    return { totalSubscribers, newSubscribersData, churnedSubscribers, growthRates };
   }
-
-  return subscribers;
-}
-
-function calculateBaselineSubscribers() {
-  const subscribers = [startingSubscribers];
-  let newSubscribers = newSubscribersPerMonth;
-
-  for (let i = 1; i <= 60; i++) {
-    let churningSubscribers = subscribers[i - 1] * (initialMonthlyChurnRate / 100);
-    subscribers[i] = subscribers[i - 1] + newSubscribers - churningSubscribers;
-  }
-
-  return subscribers;
-}
-
-function calculateYearOverYearGrowth(subscribers) {
-  const growthRates = [null, null, null, null, null, null, null, null, null, null, null, null];
-  for (let i = 12; i < subscribers.length; i++) {
-    growthRates[i] = ((subscribers[i] / subscribers[i - 12]) - 1) * 100;
-  }
-  return growthRates;
-}
-
-function calculateNewSubscribers(acquisitionGrowth, monthlyChurnRate) {
-  const newSubscribersData = [Math.round(newSubscribersPerMonth)];
-  let newSubscribers = newSubscribersPerMonth;
-
-  for (let i = 1; i <= 60; i++) {
-    newSubscribers += newSubscribers * (acquisitionGrowth / 100);
-    newSubscribersData[i] = Math.round(newSubscribers);
-  }
-
-  return newSubscribersData;
-}
-
+  
 let subscriberChart;
 
-function renderChart(data, baselineData, yoyGrowthData, newSubscribersMonthlyData) {
+function renderChart(data, yoyGrowthData, newSubscribersMonthlyData,churnedSubscribersData) {
   const ctx = document.getElementById('subscriberChart').getContext('2d');
 
   if (subscriberChart) {
@@ -61,14 +40,6 @@ function renderChart(data, baselineData, yoyGrowthData, newSubscribersMonthlyDat
     data: {
       labels: [...Array(61).keys()].map(x => x),
       datasets: [
-        {
-          label: 'Baseline',
-          data: baselineData,
-          borderColor: 'rgba(211, 211, 211, 1)',
-          borderWidth: 1,
-          pointRadius: 0,
-          yAxisID: 'y',
-        },
         {
           label: 'Projection',
           data: data,
@@ -89,112 +60,133 @@ function renderChart(data, baselineData, yoyGrowthData, newSubscribersMonthlyDat
           borderColor: 'rgba(153, 102, 255, 1)',
           borderWidth: 1,
           yAxisID: 'y',
+        },
+        {
+            label: 'Churned Subscribers',
+            data: churnedSubscribersData,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            yAxisID: 'y',
         }
       ]
     },
-      options: {
-          plugins: {
-              legend: {
-                  labels: {
-                      font: {
-                          family: 'monospace'
-                      }
-                  }
+    options: {
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                family: 'monospace'
               }
-          },
-          animation: false,
-          scales: {
-              x: {
-                  title: {
-                      display: true,
-                      text: 'Months',
-                      font: {
-                          family: 'monospace'
-                      }
-                  },
-                  beginAtZero: true,
-                  ticks: {
-                      font: {
-                          family: 'monospace'
-                      }
-                  }
-              },
-              y: {
-                  title: {
-                      display: true,
-                      text: 'Total Subscribers',
-                      font: {
-                          family: 'monospace'
-                      }
-                  },
-                  min: -100000,
-                  max: 500000,
-                  beginAtZero: false,
-                  ticks: {
-                      callback: function(value, index, values) {
-                        // Only display positive values
-                        return value >= 0 ? value : null;
-                      }
-                    }
-                  },
-              y1: {
-                  type: 'linear',
-                  position: 'right',
-                  title: {
-                      display: true,
-                      text: 'Growth Rate (%)',
-                      font: {
-                          family: 'monospace'
-                      }
-                  },
-                  beginAtZero: false,
-                  min: -20,
-                  max: 100,
-                  grid: {
-                      drawOnChartArea: false,
-                  },
-                  ticks: {
-                      font: {
-                          family: 'monospace'
-                      }
-                  }
-              }
+            }
           }
+        },
+        animation: false,
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Months',
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              }
+            },
+            beginAtZero: true,
+            ticks: {
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              }
+            },
+            grid: {
+              color: '#303030'
+            }
+          },
+          y: {
+            type: 'linear',
+            title: {
+              display: true,
+              text: 'Total Subscribers',
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              }
+            },
+            min: -100000,
+            max: 500000,
+            beginAtZero: false,
+            ticks: {
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              },
+              callback: function(value, index, values) {
+                // Only display positive values
+                return value >= 0 ? value : null;
+              }
+            },
+            grid: {
+              color: '#303030'
+            }
+          },
+          y1: {
+            type: 'linear',
+            position: 'right',
+            title: {
+              display: true,
+              text: 'Growth Rate (%)',
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              }
+            },
+            beginAtZero: false,
+            min: -20,
+            max: 100,
+            grid: {
+              drawOnChartArea: false,
+              color: '#303030'
+            },
+            ticks: {
+              font: {
+                family: 'monospace',
+                color: '#303030'
+              }
+            }
+          }
+        }
       }
-  });
-}
+    });
+  }
 
 function updateChart() {
     const acquisitionGrowth = parseFloat(document.getElementById('acquisitionGrowth').value);
     const monthlyChurnRate = parseFloat(document.getElementById('churnReduction').value);
   
-    const newSubscribersData = calculateSubscribers(acquisitionGrowth, monthlyChurnRate);
-    const baselineData = calculateBaselineSubscribers();
-    const yoyGrowthData = calculateYearOverYearGrowth(newSubscribersData);
-    const newSubscribersMonthlyData = calculateNewSubscribers(acquisitionGrowth, monthlyChurnRate);
-  
-    renderChart(newSubscribersData, baselineData, yoyGrowthData, newSubscribersMonthlyData);
-}
-  
+    const metrics = calculateMetrics(startingSubscribers, newSubscribersPerMonth, acquisitionGrowth, monthlyChurnRate);
+
+    const newSubscribersData = metrics.totalSubscribers;
+    const yoyGrowthData = metrics.growthRates;
+    const newSubscribersMonthlyData = metrics.newSubscribersData;
+    const churnedSubscribersData = metrics.churnedSubscribers;
+    
+    renderChart(newSubscribersData, yoyGrowthData, newSubscribersMonthlyData, churnedSubscribersData);
+    
+  }
+ 
   function updateBaselineData() {
     startingSubscribers = parseInt(document.getElementById('startingSubscribers').value);
     newSubscribersPerMonth = parseInt(document.getElementById('netNewSubscribers').value);
-    initialMonthlyChurnRate = parseFloat(document.getElementById('initialMonthlyChurn').value);
 }
-  
+
     document.getElementById('acquisitionGrowth').oninput = function() {
         document.getElementById('acquisitionOutput').textContent = parseFloat(this.value).toFixed(1);
         let a = parseFloat(this.value);
         let b = 1 + (a / 100);
         let c = b ** 12;
         let d = (c - 1) * 100;
-        document.getElementById('yearlyAcquisitionOutput').textContent = d;
+        document.getElementById('yearlyAcquisitionOutput').textContent = parseFloat(d).toFixed(0);
         updateChart();
-        if (this.value > 3) {
-            this.classList.add('bg-red-500');
-        } else {
-            this.classList.remove('bg-red-500');
-        }
     }
     
     document.getElementById('churnReduction').oninput = function() {
@@ -203,13 +195,8 @@ function updateChart() {
         let b = 1 + (a / 100);
         let c = b ** 12;
         let d = (c - 1) * 100;
-        document.getElementById('yearlyChurnOutput').textContent = d;
+        document.getElementById('yearlyChurnOutput').textContent = parseFloat(d).toFixed(0);
     updateChart();
-    if (this.value < 2) {
-        this.classList.add('bg-red-500');
-    } else {
-        this.classList.remove('bg-red-500');
-    }
     }
     
     window.onload = () => {
